50e33530af83081021fa8d98d9cb678a1618c915
ChrisVct <vichit.christophe@gmail.com>
2022-05-23 21:02:43 +0200

    premier commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
+import NotreHistoire from "./components/Main/NotreHistoire/NotreHistoire";
+import LaBrasserie from "./components/Main/LaBrasserie/LaBrasserie";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container-fluid g-0">
+      <header>
+        <NavBar />
       </header>
+      <main>
+        <div className="fixed-bg bg1"></div>
+        <NotreHistoire />
+        <div className="fixed-bg bg2"></div>
+        <LaBrasserie />
+        <div className="fixed-bg bg3"></div>
+        <NosBieres />
+        <section id="nosBieres"></section>
+        <div className="fixed-bg bg4"></div>
+        <section id="actualites"></section>
+        <div className="fixed-bg bg5"></div>
+        <section id="pointsDeVente"></section>
+        <div className="fixed-bg bg6"></div>
+        <section id="Contact"></section>
+        <div className="fixed-bg bg7"></div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
