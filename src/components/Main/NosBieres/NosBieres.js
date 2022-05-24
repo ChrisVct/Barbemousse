16cc8747c9c75bc498c9b25addf2574931648dd7
ChrisVct <vichit.christophe@gmail.com>
2022-05-24 22:29:48 +0200

    Mise en page responsive de NosBieres

--- src/components/Main/NosBieres/NosBieres.js
+++ src/components/Main/NosBieres/NosBieres.js
@@ -10,25 +10,22 @@ export default function NosBieres() {
     <>
       <div className="container-fluid g-0" id="NosBieresWrapper">
         <section
-          className="nosBieres-container row col-6 col-xl-5 offset-xl-1 d-flex flex-row g-0 "
+          className="nosBieres-container row col-12 col-xl-5 offset-xl-1 d-flex flex-row g-0 "
           id="nosBieres"
         >
-          <figure className="beerCard1 beerCard col-3">
+          <figure className="beerCard1 beerCard col-6 col-xl-3 order-first">
             <img src={Biere1} alt="" />
           </figure>
-          <figure className="beerCard2 beerCard col-3">
+          <figure className="beerCard2 beerCard col-6 col-xl-3 order-2">
             <img src={Biere2} alt="" />
           </figure>
-          <figure className="beerCard3 beerCard col-3">
+          <figure className="beerCard3 beerCard col-6 col-xl-3 order-3 ">
             <img src={Biere3} alt="" />
           </figure>
-          <figure className="beerCard4 beerCard col-3">
+          <figure className="beerCard4 beerCard col-6 col-xl-3 order-last">
             <img src={Biere4} alt="" />
           </figure>
-          <article className="beerPresentation0 beerPresentation col-6 col-xl-4">
-            <h3>Sélectionner votre bière pour en savoir plus !</h3>
-          </article>
-          <article className="beerPresentation1 beerPresentation col-6 col-xl-4">
+          <article className="beerPresentation1 beerPresentation col-6 col-xl-4 order-0">
             <h3>La moussette</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
@@ -39,14 +36,41 @@ export default function NosBieres() {
               enim libero, sequi ad!
             </p>
           </article>
-          <article className="beerPresentation2 beerPresentation col-6 col-xl-4">
-            <h3>Beer2</h3>
+          <article className="beerPresentation2 beerPresentation col-6 col-xl-4 order-1">
+            <h3>L'embrumée</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+              aliquid vitae dolor eaque unde, ipsa veniam at suscipit, neque
+              tempore facere blanditiis et sunt similique cupiditate illo dicta
+              delectus, velit ut. Atque impedit quia quibusdam eius totam,
+              tenetur adipisci obcaecati odit doloribus nobis minus suscipit qui
+              enim libero, sequi ad!
+            </p>
           </article>
-          <article className="beerPresentation3 beerPresentation col-6 col-xl-4">
-            <h3>Beer3</h3>
+          <article className="beerPresentation3 beerPresentation col-6 col-xl-4 order-4">
+            <h3>La pression sociale</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+              aliquid vitae dolor eaque unde, ipsa veniam at suscipit, neque
+              tempore facere blanditiis et sunt similique cupiditate illo dicta
+              delectus, velit ut. Atque impedit quia quibusdam eius totam,
+              tenetur adipisci obcaecati odit doloribus nobis minus suscipit qui
+              enim libero, sequi ad!
+            </p>
           </article>
-          <article className="beerPresentation4 beerPresentation col-6 col-xl-4">
-            <h3>Beer4</h3>
+          <article className="beerPresentation4 beerPresentation col-6 col-xl-4 order-5">
+            <h3>La canonnade</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+              aliquid vitae dolor eaque unde, ipsa veniam at suscipit, neque
+              tempore facere blanditiis et sunt similique cupiditate illo dicta
+              delectus, velit ut. Atque impedit quia quibusdam eius totam,
+              tenetur adipisci obcaecati odit doloribus nobis minus suscipit qui
+              enim libero, sequi ad!
+            </p>
+          </article>
+          <article className="beerPresentation0 beerPresentation col-6 col-xl-4 d-none d-xl-block ">
+            <h3>Sélectionner votre bière pour en savoir plus !</h3>
           </article>
         </section>
       </div>
