89cc94b9618185cad0c4d8e9a325be234760c86b
ChrisVct <vichit.christophe@gmail.com>
2022-05-25 21:47:59 +0200

    Fin responsive Actualités

--- src/components/Main/Facebook/FacebookSection.js
+++ src/components/Main/Facebook/FacebookSection.js
@@ -6,7 +6,7 @@ export default function FacebookSection() {
     <div className="container-fluid">
       <section className="mainArticle row" id="fbSection">
         <div
-          className="fb-like-box col-4 offset-2"
+          className="fb-like-box col-4 d-none d-md-block offset-lg-1 offset-xl-2"
           data-href="https://www.facebook.com/Microbrasserie-Barbemousse-109357753889418/"
           data-width="400"
           data-height="600"
@@ -16,9 +16,15 @@ export default function FacebookSection() {
           data-stream="true"
           data-show-border="false"
         ></div>
-        <article className="col-4">
+        <article className="col- 12 col-md-4 offset-md-3 offset-lg-2 offset-xl-1">
           <i className="fa-brands fa-facebook "></i>
-          <h2>Retrouvez toute l'actualité sur notre page Facebook !</h2>
+          <h2>
+            Retrouvez toute l'actualité sur{" "}
+            <a href="https://www.facebook.com/Microbrasserie-Barbemousse-109357753889418/">
+              notre page Facebook
+            </a>{" "}
+            !
+          </h2>
           <h4>
             Suivez toute l’actualité de la Brasserie sur notre page Facebook.
             N’hésitez pas à liker et à partager !
