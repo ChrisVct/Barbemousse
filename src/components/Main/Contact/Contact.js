485160073177ff15f99da570913a763536ef7822
ChrisVct <vichit.christophe@gmail.com>
2022-05-28 21:57:11 +0200

    param du script php Contact.js

--- src/components/Main/Contact/Contact.js
+++ src/components/Main/Contact/Contact.js
@@ -4,47 +4,57 @@ import "./Contact.scss";
 export default function Contact() {
   return (
     <section id="contact" className="row">
-      <section className="col-4 offset-2" style={{ border: "2px solid black" }}>
+      <section className="col-4 offset-2 my-3">
         <p>text ici</p>
       </section>
-      <form action="post" className="col-4  my-3">
+      <form
+        method="post"
+        action="/traitement_formulaire.php"
+        className="col-4  my-3"
+        id="contactForm"
+      >
+        {/* id="contact" method="post" action="/traitement_formulaire.php" */}
         <div className="row col-12 g-1">
           <div className="col mb-0 form-floating ">
             <input
               type="text"
               className="form-control form-control-sm"
-              id="floatingNom"
+              id="nom"
               placeholder=" "
+              name="nom"
             />
-            <label htmlFor="floatingNom">Nom</label>
+            <label htmlFor="nom">Nom</label>
           </div>
           <div className="col mb-0 form-floating  ">
             <input
               type="text"
               className="form-control form-control-sm"
-              id="floatingPrenom"
+              id="prenom"
               placeholder=" "
+              name="prenom"
             />
-            <label htmlFor="floatingPrenom"> Prénom</label>
+            <label htmlFor="prenom"> Prénom</label>
           </div>
         </div>
         <div className="mb-0 form-floating my-3 ">
           <input
             type="email"
             className="form-control"
-            id="floatingEmail"
+            id="email"
             placeholder=" "
+            name="email"
           />
-          <label htmlFor="floatingEmail">Courriel</label>
+          <label htmlFor="email">Courriel</label>
         </div>
         <div className="form-floating my-3">
           <textarea
             className="form-control"
             placeholder=" "
-            id="floatingTextarea"
+            id="message"
+            name="message"
             style={{ height: "200px" }}
           ></textarea>
-          <label htmlFor="floatingTextarea">Votre message</label>
+          <label htmlFor="message">Votre message</label>
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
