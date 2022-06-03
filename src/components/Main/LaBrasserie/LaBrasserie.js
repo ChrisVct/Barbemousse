c805979396653a1d5206a53711f4bef88d2f299d
ChrisVct <vichit.christophe@gmail.com>
2022-06-03 21:43:02 +0200

    ajout map, text et pics notreHistoire et laBrasserie

--- src/components/Main/LaBrasserie/LaBrasserie.js
+++ src/components/Main/LaBrasserie/LaBrasserie.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./LaBrasserie.scss";
-import BreweryPics1 from "./../../../Assests/breweryPics1.jpg";
+import brasseriePics from "./../../../Assests/brasseriePics.png";
 
 export default function LaBrasserie() {
   return (
@@ -11,18 +11,25 @@ export default function LaBrasserie() {
           className=" row col-12 col-xl-8 offset-xl-2 g-0 d-flex flex-column-reverse flex-md-row-reverse "
         >
           <figure className="col-12 col-md-6 ">
-            <img src={BreweryPics1} alt="" />
+            <img src={brasseriePics} alt="" />
           </figure>
           <article className="col-12 col-md-6 px-3 pt-3">
             <h1>La Brasserie</h1>
             <p>
-              &emsp;&emsp;Lorem ipsum, dolor sit amet consectetur adipisicing
-              elit. Expedita dicta, eius quam provident odit voluptas iusto
-              laboriosam delectus accusantium reiciendis veniam quasi cum iure
-              molestias assumenda nemo alias in dolor nihil nostrum error saepe
-              laborum suscipit nobis? Hic inventore necessitatibus quos nemo
-              dolore animi asperiores placeat iure? Repellendus, itaque
-              veritatis!
+              &emsp;&emsp;La brasserie est installée à Bonnemaison, dans un lieu
+              paisible où il fait bon vivre aux portes de la suisse normande.
+              Elle se compose d’une salle de brassage, d’une salle de
+              fermentation, d’une boutique et de zones de stockage, Je vous y
+              propose des bières originales et brassées avec passion selon une
+              méthode artisanale. La brasserie est en perpétuelle évolution avec
+              pour objectifs principaux de vous proposer des bières de qualité,
+              innovantes et dans le respect de l’environnement. La majorité des
+              déchets de la brasserie est par exemple revalorisée sur place
+              (drèches de brasserie et résidus de houblons compostés ou pour
+              l’alimentation de nos poules et chèvres, traitement des eaux par
+              phyto épuration…) Un jardin potager fonctionnant sur les bases de
+              la permaculture est également en création sur notre terrain et est
+              intimement lié à la brasserie.
             </p>
           </article>
         </section>
--- src/components/Main/NotreHistoire/NotreHistoire.js
+++ src/components/Main/NotreHistoire/NotreHistoire.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./NotreHistoire.scss";
-import notreHistoirePics1 from "./../../../Assests/notreHistoirePics1.jpg";
+import histoirePics from "./../../../Assests/histoirePics.jpg";
 
 export default function NotreHistoire() {
   return (
@@ -11,18 +11,20 @@ export default function NotreHistoire() {
           className=" row col-12 col-xl-8 offset-xl-2 g-0 d-flex flex-column-reverse flex-md-row"
         >
           <figure className="col-12 col-md-6 ">
-            <img src={notreHistoirePics1} alt="" />
+            <img src={histoirePics} alt="" />
           </figure>
           <article className="col-12 col-md-6 px-3 pt-3">
-            <h1>Notre histoire</h1>
+            <h1>L’histoire de la brasserie</h1>
             <p>
-              &emsp;&emsp;Lorem ipsum, dolor sit amet consectetur adipisicing
-              elit. Expedita dicta, eius quam provident odit voluptas iusto
-              laboriosam delectus accusantium reiciendis veniam quasi cum iure
-              molestias assumenda nemo alias in dolor nihil nostrum error saepe
-              laborum suscipit nobis? Hic inventore necessitatibus quos nemo
-              dolore animi asperiores placeat iure? Repellendus, itaque
-              veritatis!
+              &emsp;&emsp;Passionné depuis longtemps par le monde brassicole,
+              j’ai commencé à brasser mes premières bières en 2014 dans le
+              garage familial. S’en est suivi un long apprentissage passant par
+              pas mal d’essais et notamment par du bénévolat dans une petite
+              brasserie locale. Désireux de faire partager ma passion et d’en
+              vivre, c’est en 2019 que j’ouvre enfin ma propre brasserie à
+              Bonnemaison. Après trois ans de rénovation d’une ancienne ferme
+              dans un petit coin tranquille aux portes de la suisse normande,
+              cet endroit deviendra mon lieu de travail et lieu de vie.
             </p>
           </article>
         </section>
