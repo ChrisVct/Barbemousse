35c888318d37aa8a2360b55967fbef9b4f8eeded
ChrisVct <vichit.christophe@gmail.com>
2022-06-16 20:46:29 +0200

    Revert "footer en bstp au lieu de react bstp"
    This reverts commit 57c7e559c8b526391485a15c21c0d4af02ba2596.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Modal, OverlayTrigger, Popover } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 export default function Footer() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <div
@@ -8,95 +14,53 @@ export default function Footer() {
         style={{ height: "50px", backgroundColor: "#6d594d", color: "#f1ecd5" }}
       >
         <p className="m-0">© Microbrasserie Barbemousse 2022 -&nbsp; </p>
-        <button
-          type="button"
-          className="btn btn-link p-0"
-          data-bs-toggle="modal"
-          data-bs-target="#mentionsLegales"
+        <Button
+          variant="link p-0"
+          onClick={handleShow}
           style={{
             color: "#f1ecd5",
           }}
         >
           Mentions légales
-        </button>
+        </Button>
         <p className="m-0">. Site réalisé par&nbsp;</p>
-        <button
-          type="button"
-          className="btn btn-link p-0"
-          data-bs-toggle="modal"
-          data-bs-target="#christopheVichitModal"
-          style={{
-            color: "#f1ecd5",
-          }}
+        <OverlayTrigger
+          trigger="click"
+          key="top"
+          placement="top"
+          overlay={
+            <Popover id={`popover-positioned-top`}>
+              <Popover.Header as="h3">{`Contactez-moi !`}</Popover.Header>
+              <Popover.Body>vichit.christophe@gmail.com</Popover.Body>
+            </Popover>
+          }
         >
-          Christophe VICHIT.
-        </button>
+          <Button
+            variant="link p-0 "
+            style={{
+              color: "#f1ecd5",
+            }}
+          >
+            Christophe VICHIT.
+          </Button>
+        </OverlayTrigger>
       </div>
-      <div
-        className="modal fade "
-        id="mentionsLegales"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-        style={{
-          padding: "0",
-        }}
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Mentions légales
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              Raison sociale : <br />
-              R.C.S : <br />
-              SIRET : <br />
-              Capital :<br />
-              Date de Création : <br />
-              Siège social :<br />
-              Téléphone :
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        className="modal fade"
-        id="christopheVichitModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Contact
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              Site personnel en cours de construction. Vous pouvez me contacter
-              à l'adresse suivante : <br />
-              <br /> <strong>vichit.christophe@gmail.com</strong>
-              <br />
-              <br />
-            </div>
-          </div>
-        </div>
-      </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Mentions légales</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {" "}
+          Raison sociale : <br />
+          R.C.S : <br />
+          SIRET : <br />
+          Capital :<br />
+          Date de Création : <br />
+          Siège social :<br />
+          Téléphone :
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
