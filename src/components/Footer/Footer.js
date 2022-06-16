5e3edb2878a0f37d76ac765920fab4f13680ad9f
ChrisVct <vichit.christophe@gmail.com>
2022-06-16 21:06:42 +0200

    ajout modal pour 'Christophe VICHIT'

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,14 @@ import { Modal, OverlayTrigger, Popover } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 export default function Footer() {
+  //toogle "mentions variables"
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  //toggle "Christophe VICHIT"
+  const [show2, setShow2] = useState(false);
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
   return (
     <>
       <div
@@ -24,26 +28,15 @@ export default function Footer() {
           Mentions légales
         </Button>
         <p className="m-0">. Site réalisé par&nbsp;</p>
-        <OverlayTrigger
-          trigger="click"
-          key="top"
-          placement="top"
-          overlay={
-            <Popover id={`popover-positioned-top`}>
-              <Popover.Header as="h3">{`Contactez-moi !`}</Popover.Header>
-              <Popover.Body>vichit.christophe@gmail.com</Popover.Body>
-            </Popover>
-          }
+        <Button
+          variant="link p-0"
+          onClick={handleShow2}
+          style={{
+            color: "#f1ecd5",
+          }}
         >
-          <Button
-            variant="link p-0 "
-            style={{
-              color: "#f1ecd5",
-            }}
-          >
-            Christophe VICHIT.
-          </Button>
-        </OverlayTrigger>
+          Christophe VICHIT.
+        </Button>
       </div>
 
       <Modal show={show} onHide={handleClose}>
@@ -52,6 +45,7 @@ export default function Footer() {
         </Modal.Header>
         <Modal.Body>
           {" "}
+          <h3>Entreprise</h3>
           Raison sociale : <br />
           R.C.S : <br />
           SIRET : <br />
@@ -59,6 +53,33 @@ export default function Footer() {
           Date de Création : <br />
           Siège social :<br />
           Téléphone :
+          <br />
+          <br />
+          <h3>Hébergement</h3>
+          Ce site est hébergé sur....
+          <br />
+          <br />
+          <h3>Crédits</h3>
+          Crédits image :<br />
+          https://www.pixabay.com/fr/ // Auteur : sirmarkdavid
+          <br />
+          Le photographe des bières
+          <br />
+          <br />
+        </Modal.Body>
+      </Modal>
+      <Modal show={show2} onHide={handleClose2}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contact</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Mon site personnel est en cours de réalisation. En attendant, vous
+          pouvez me contacter via :
+          <br />
+          <br />
+          vichit.christophe@gmail.com
+          <br />
+          <br />
         </Modal.Body>
       </Modal>
     </>
